a9dce8b685957f0d58df8c267a06b07562899c1c
MLeandroCardenas <mleandrocardenas@ucundinamarca.edu.co>
2020-09-22 15:22:55 -0500

    nuevooooo

--- src/main/java/com/udec/repository/AlumnoRepoImpl.java
+++ src/main/java/com/udec/repository/AlumnoRepoImpl.java
@@ -20,10 +20,22 @@ import com.udec.dto.AlumnoDto;
 @Repository
 public class AlumnoRepoImpl implements IAlumnoRepo {
 
+	// bd remota
+	
 	private final String rutaFichero = "alumno.txt";
-	private final String url = "jdbc:postgresql:postgres://uffwdowxltlinq:b440c1e2b86259a6f742f6d9722a7ad7a1021d47f56848d0a18f820ffe88710b@ec2-34-234-185-150.compute-1.amazonaws.com:5432/d8rqmiutr18q6i";
-    private final String user = "uffwdowxltlinq";
-    private final String password = "b440c1e2b86259a6f742f6d9722a7ad7a1021d47f56848d0a18f820ffe88710b";
+	private final String url = "jdbc:postgresql://postgres://uffwdowxltlinq:b440c1e2b86259a6f742f6d9722a7ad7a1021d47f56848d0a18f820ffe88710b@ec2-34-234-185-150.compute-1.amazonaws.com:5432/d8rqmiutr18q6i";
+	private final String user = "uffwdowxltlinq";
+	private final String password = "b440c1e2b86259a6f742f6d9722a7ad7a1021d47f56848d0a18f820ffe88710b";
+    
+	
+	/*
+	private final String rutaFichero = "C:\\Users\\michl\\OneDrive\\Documentos\\springBoot\\serviciosArchivoDoc\\src\\main\\resources\\static\\alumno.txt";
+	private final String url = "jdbc:postgresql://localhost/testing";
+    private final String user = "postgres";
+    private final String password = "1234";
+    */
+    
+    
 	private List<AlumnoDto> listaAlumno;
 	private List<AlumnoDto> serializar;
 	private List<AlumnoDto> deserializar;
